5b1c8752bcdad0c14d59f873dc79f3b8be19cf35
Ghantoos7 <ghnatios1@gmail.com>
2024-04-18 00:23:51 +0300

    FrontEnd <> Added additional stack logos

--- client/src/components/Projectcard/Projectcard.jsx
+++ client/src/components/Projectcard/Projectcard.jsx
@@ -4,11 +4,31 @@ import './Projectcard.css';
 import AWSLogo from '../../assets/logos/AWS.svg';
 import PythonLogo from '../../assets/logos/Python.svg';
 import PalantirFoundryLogo from '../../assets/logos/Palantir_Foundry.svg';
+import TensorFlowLogo from '../../assets/logos/tensorflow.svg';
+import KubernetesLogo from '../../assets/logos/kubernets.svg';
+import DockerLogo from '../../assets/logos/docker.svg';
+import SparkLogo from '../../assets/logos/apache-spark.svg';
+import HadoopLogo from '../../assets/logos/hadoop.svg';
+import PostgreSQLLogo from '../../assets/logos/postgresql.svg';
+import ReactLogo from '../../assets/logos/react.svg';
+import ReduxLogo from '../../assets/logos/redux.svg';
+import CSSLogo from '../../assets/logos/css.svg';
+import TerraformLogo from '../../assets/logos/terraform-enterprise.svg';
 
 const techLogos = {
   'AWS': AWSLogo,
   'Python': PythonLogo,
   'Palantir Foundry': PalantirFoundryLogo,
+  'TensorFlow': TensorFlowLogo,
+  'Kubernetes': KubernetesLogo,
+  'Docker': DockerLogo,
+  'Spark': SparkLogo,
+  'Hadoop': HadoopLogo,
+  'PostgreSQL': PostgreSQLLogo,
+  'React': ReactLogo,
+  'Redux': ReduxLogo,
+  'CSS': CSSLogo,
+  'Terraform': TerraformLogo,
 };
 
 function Projectcard({ title, description, technologies, image }) {
