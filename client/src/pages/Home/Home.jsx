82eb04d42542cb62214830f6988dcdd3b4bbb961
Ghantoos7 <ghnatios1@gmail.com>
2024-04-17 21:17:32 +0300

    FrontEnd <> Made home section more responsive and changed design a bit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,12 +3,9 @@ import './Home.css';
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="left">
-        <div className="subheader">
-          Who Are We?
-        </div>
-        <div className="content">
+    <div className="home-container">
+      <div className="content-box">
+        <div className="text-content">
           <h1>Welcome to ScrapBusters Inc.</h1>
           <p>Leading the digitalization in quality management.</p>
         </div>
