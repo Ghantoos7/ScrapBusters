cf7667759e5b90a614faab9e940e7a85ceafd189
Ghantoos7 <ghnatios1@gmail.com>
2024-04-18 00:25:10 +0300

    FrontEnd <> Adjusted navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+import './Navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-links">
+        <li>
+          <Link to="home" spy={true} smooth={true} offset={-40} duration={500}>Home</Link>
+        </li>
+        <li>
+          <Link to="projects" spy={true} smooth={true} offset={-40} duration={500}>Projects</Link>
+        </li>
+        <li>
+          <Link to="team" spy={true} smooth={true} offset={-40} duration={500}>Team</Link>
+        </li>
+        <li>
+          <Link to="faq" spy={true} smooth={true} offset={-40} duration={500}>FAQ</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
