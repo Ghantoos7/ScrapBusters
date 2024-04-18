dd00e677fb8cee59562c28f8be3c18ea3ecc3427
Ghantoos7 <ghnatios1@gmail.com>
2024-04-18 17:15:13 +0300

    FrontEnd <> Adjusted colors and added small details

--- client/src/pages/Team/Team.jsx
+++ client/src/pages/Team/Team.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import TeamMemberCard from '../../components/Teammembercard/Teammembercard';
 import './Team.css';
 
-import { Navigation} from 'swiper/modules';
+import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/swiper-bundle.css';
@@ -18,36 +18,36 @@ import Mysteryguy from '../../assets/Mysteryguy.jpg'
 
 const teamMembers = [
   {
-      name: 'Ishansh',
-      title: 'Senior Data Scientist',
-      message: 'Welcome message 1',
-      quote: 'Quote 1',
-      image: IshanshImage
+    name: 'Ishansh',
+    title: 'Senior Data Scientist',
+    message: 'Welcome message 1',
+    quote: 'Quote 1',
+    image: IshanshImage
   },
 
   {
-      name: 'Louis',
-      title: 'Senior Data Scientist',
-      message: 'Welcome message 2',
-      quote: 'Quote 2',
-      image: LouisImage
+    name: 'Louis',
+    title: 'Senior Data Scientist',
+    message: 'Welcome message 2',
+    quote: 'Quote 2',
+    image: LouisImage
   },
 
   {
-      name: '?????????',
-      title: 'Software Engineering Intern',
-      message: 'Welcome message 4',
-      quote: 'Quote 4',
-      image: Mysteryguy
+    name: '?????????',
+    title: 'Software Engineering Intern',
+    message: 'Welcome message 4',
+    quote: 'Quote 4',
+    image: Mysteryguy
   },
-  
+
   {
     name: 'Sharbel',
     title: 'Senior Data Science Intern',
     message: 'Welcome message 3',
     quote: 'Quote 3',
     image: SharbelImage
-},
+  },
 
 
 ];
@@ -58,20 +58,27 @@ function Team() {
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 769);
     window.addEventListener("resize", handleResize);
-    handleResize(); 
+    handleResize();
     return () => window.removeEventListener("resize", handleResize);
-}, []);
+  }, []);
 
   return (
     <div className="team-container">
-      <h1>Team</h1>
+      <div className="header-container">
+        <h1><span className="arrow">&gt;</span>Team</h1>
+        <div className="guru-container">
+          <span className="circle circle1"></span>
+          <span className="circle circle2"></span>
+          <span className="circle circle3"></span>
+        </div>
+      </div>
       {isMobile ? (
         teamMembers.map((member, index) => (
           <TeamMemberCard key={index} {...member} />
         ))
       ) : (
         <Swiper
-        className="team-swiper"
+          className="team-swiper"
           modules={[Navigation]}
           spaceBetween={30}
           slidesPerView={3}
