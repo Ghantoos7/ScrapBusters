61d070d6a7de360d465803c19ad0a820a7b47a10
Ghantoos7 <ghnatios1@gmail.com>
2024-04-19 06:04:02 +0300

    FrontEnd <> Added project images

--- client/src/pages/Projects/Projects.jsx
+++ client/src/pages/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Projectcard from '../../components/Projectcard/Projectcard';
 import './Projects.css';
-import Image1 from './junk-yard-cars-yard-junk.jpg';
 
 import { Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,50 +9,56 @@ import 'swiper/swiper-bundle.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-
+import image1 from '../../assets/rotor-production.jpg'
+import image2 from '../../assets/stator.jpg'
+import image3 from '../../assets/scrap-rate.jpg'
+import image4 from '../../assets/predictiveanalytics.jpg'
+import image5 from '../../assets/cloud.jpg'
+import image6 from '../../assets/database.jpg'
+import image7 from '../../assets//home.png';
 
 const projects = [
   {
     title: 'Rotor Production Efficiency',
     description: 'We are enhancing rotor production efficiency by leveraging Palantir Foundry and AWS.',
     technologies: ['Palantir Foundry', 'AWS', 'Python'],
-    image: Image1,
+    image: image1,
   },
   {
     title: 'Stator Rework Reduction',
     description: 'Our team is reducing rework in stator production using data-driven analytics.',
     technologies: ['TensorFlow', 'Kubernetes', 'Docker'],
-    image: Image1,
+    image: image2,
   },
   {
     title: 'Scrap Rate Minimization',
     description: 'We are minimizing scrap rates in component production with robust data pipelines.',
     technologies: ['Python', 'Spark', 'Hadoop'],
-    image: Image1,
+    image: image3,
   },
   {
     title: 'Predictive Analytics Deployment',
     description: 'We are deploying a machine learning model for predictive analytics to foresee production issues.',
     technologies: ['TensorFlow', 'Kubernetes', 'Docker'],
-    image: Image1,
+    image: image4,
   },
   {
     title: 'Cloud Infrastructure for Quality Management',
     description: 'We are setting up a cloud infrastructure to support our digitalization efforts in quality management.',
     technologies: ['AWS', 'Terraform', 'Docker'],
-    image: Image1,
+    image: image5,
   },
   {
     title: 'Database Optimization for Production Data',
     description: 'We are optimizing database queries for improved performance in handling production data.',
     technologies: ['PostgreSQL', 'Python', 'Docker'],
-    image: Image1,
+    image: image6,
   },
   {
     title: 'Responsive Web Application for Quality Management',
     description: 'We are developing a responsive web application to introduce our quality management team.',
     technologies: ['React', 'Python', 'CSS'],
-    image: Image1,
+    image: image7,
   },
 ];
 
